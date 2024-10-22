17946225837a122d0d1f87b8778e50816e97d3f5
Weskerty <marcoygor0@gmail.com>
2024-10-22 01:07:28 -0300

    test

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -5,8 +5,6 @@ const handler = (m) => m;
 handler.all = async function(m, {conn}) {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.nv_global
 
   const chat = global.db.data.chats[m.chat];
 
